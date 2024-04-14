f610f4f8a59e21e7cc8fe0fef8152dd9d948b8b7
Said Gabriel <163392959+gabri203@users.noreply.github.com>
2024-04-14 19:58:28 +0200

    Update substitute.c

--- substitute.c
+++ substitute.c
@@ -7,21 +7,21 @@ void substitute(char *s1, char *s2, int m){
 	char *ps1 = s1;
 	char *ps2 = s2;
 	while(*ps2!='\0'){
-		cont_s2++;
-		ps2++;
+		     cont_s2++;
+		     ps2++;
 	}
 	ps2=s2;
 	if(cont_s2>m){
-		ps1=s1;
+		  ps1=s1;
 	}
 	else if(cont_s2<m){
 	for(i=1;i<m;i++){
-	ps1++;
+	    ps1++;
 	}
 	while(*ps2!='\0'){
-	*ps1=*ps2;
-	ps1++;
-	ps2++;
+	    *ps1=*ps2;
+	     ps1++;
+	     ps2++;
 	}	
 	}
 	
@@ -29,7 +29,7 @@ void substitute(char *s1, char *s2, int m){
 int main(int argc,char *argv[]){
 	
 	if(argc!=4){
-		printf("Hey bro,devi fare solo 3 inserimenti nella linea di comando \n",argv[0]);
+		  printf("Hey bro,devi fare solo 3 inserimenti nella linea di comando \n",argv[0]);
 			return 1;
 	}
 	
